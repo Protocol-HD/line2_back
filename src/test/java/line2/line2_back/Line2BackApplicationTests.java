7f6517b81d9a3349b757e3a0bdb9b75014e050d2
HD <gudehdu10@gmail.com>
2022-02-25 22:18:28 +0900

    first commit

---
+++ src/test/java/line2/line2_back/Line2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package line2.line2_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Line2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

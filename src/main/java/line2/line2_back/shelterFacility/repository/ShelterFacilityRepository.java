0d7d5e08d5ec2916cbdfd77e593bd080185dd6c4
HD <gudehdu10@gmail.com>
2022-03-11 12:08:03 +0900

    LINE2-89 add shelterFacility repository

---
+++ src/main/java/line2/line2_back/shelterFacility/repository/ShelterFacilityRepository.java
@@ -0,0 +1,7 @@
+package line2.line2_back.shelterFacility.repository;
+
+import line2.line2_back.shelterFacility.model.ShelterFacility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShelterFacilityRepository extends JpaRepository<ShelterFacility, Long> {
+}

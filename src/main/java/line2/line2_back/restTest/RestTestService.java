7df2593247cafe24e1f7e6960e121393c1b24857
hd <gudehdu10@gmail.com>
2022-04-04 11:09:12 +0900

    test rest template

---
+++ src/main/java/line2/line2_back/restTest/RestTestService.java
@@ -0,0 +1,10 @@
+package line2.line2_back.restTest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RestTestService {
+    public String test() {
+        return "test success";
+    }
+}
